d5b0d67aa46b08d1eccbe4e176048c6bcac38dbd
turulomio <turulomio@yahoo.es>
2025-05-23 08:14:15 +0200

    Removed innecesary files

--- filenamelength/poethepoet.py
+++ filenamelength/poethepoet.py
@@ -2,7 +2,6 @@ from datetime import date
 from filenamelength import __version__
 from gettext import install, translation, gettext
 from os import system
-from mangenerator import Man
 
 _=gettext
 
@@ -28,32 +27,7 @@ def release():
     print(_("  * Upload to portage repository")) 
 
 def translate():
-        #es
-        system("xgettext -L Python --no-wrap --no-location --from-code='UTF-8' -o filenamelength/locale/filenamelength.pot filenamelength/*.py")
-        system("msgmerge -N --no-wrap -U filenamelength/locale/es.po filenamelength/locale/filenamelength.pot")
-        system("msgfmt -cv -o filenamelength/locale/es/LC_MESSAGES/filenamelength.mo filenamelength/locale/es.po")
-        for language in ["en", "es"]:
-            mangenerator(language)
-
-def mangenerator(language):
-    """
-        Create man pages for parameter language
-    """
-    if language=="en":
-        install('filenamelength', 'badlocale')
-        man=Man("man/man1/filenamelength")
-    else:
-        lang1=translation('filenamelength', 'filenamelength/locale', languages=[language])
-        lang1.install()
-        man=Man("man/es/man1/filenamelength")
-    print("  - DESCRIPTION in {} is {}".format(language, _("DESCRIPTION")))
-
-    man.setMetadata("filenamelength",  1,   date.today(), "Mariano Muñoz", _("Admin options to work with the max length of the name of your files"))
-    man.setSynopsis("""usage: filenamelength [-h] [--version]
-                    [--create_examples | --remove_examples | --minimum MINIMUM]
-                    [--order_by_length]""")
-    man.header(_("DESCRIPTION"), 1)
-    man.paragraph(_("This app has the following mandatory parameters:"), 1)
-    man.paragraph("--create_example", 2, True)
-    
-    man.paragraph(_("With --pretend and --remove you can use this parameters:"), 1)
+    #es
+    system("xgettext -L Python --no-wrap --no-location --from-code='UTF-8' -o filenamelength/locale/filenamelength.pot filenamelength/*.py")
+    system("msgmerge -N --no-wrap -U filenamelength/locale/es.po filenamelength/locale/filenamelength.pot")
+    system("msgfmt -cv -o filenamelength/locale/es/LC_MESSAGES/filenamelength.mo filenamelength/locale/es.po")
