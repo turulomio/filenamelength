1f6d628deb228c1deb8d669e494c4def57d001f0
turulomio <turulomio@yahoo.es>
2025-12-27 09:32:01 +0100

    filenamelength-1.1.0

--- filenamelength/__init__.py
+++ filenamelength/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-__version__ = '1.0.0'
-__versiondatetime__=datetime(2025,5,23,8,36)
+__version__ = '1.1.0'
+__versiondatetime__=datetime(2025,12,27,9,36)
 __versiondate__=__versiondatetime__.date()
